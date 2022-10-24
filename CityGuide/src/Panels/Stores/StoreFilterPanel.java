6b2d9f6bc22a7354f7b1607b01784e993caab9ea
Iakapo410 <93939448+Iakapo410@users.noreply.github.com>
2022-10-24 20:45:16 +0300

    Merge branch 'Filters' of C:\Users\Απόστολος Ι\Documents\GitHub\CityGuide with conflicts.

--- CityGuide/src/Panels/Stores/StoreFilterPanel.java
+++ CityGuide/src/Panels/Stores/StoreFilterPanel.java
@@ -3,4 +3,24 @@ package Panels.Stores;
 import javax.swing.*;
 
 public class StoreFilterPanel extends JPanel {
+    JButton applyFilters;
+    JTextField searchField;
+    JLabel searchLabel;
+    JTextField textField1;
+    JLabel typeLabel;
+    JComboBox comboBox6;
+    JLabel ratingLabel;
+    JLabel deliveryLabel;
+    JCheckBox checkBox2;
+
+    public StoreFilterPanel()
+    {
+        applyFilters=new JButton();
+        searchField=new JTextField();
+        textField1 = new JTextField();
+        comboBox6 = new JComboxBox();
+        checkBox2 = new JCheckBoc();
+
+    }
+
 }
