b1e5213867e6c0d59c0f3b6f6c61839f7f02fcce
George <93939735+gsaraptsis@users.noreply.github.com>
2022-10-25 01:37:04 +0300

    swing GUI and import the log in form
    Add in StoresSinglePanel.java the rating label
    Create the same GUI grid in ActivitiesSinglePanel.java
    Add the RegistrationForm (from the previous project Log_In) plus the terms of service checkbox
    Add the LoginPage (from the previous project Log_In) make the correct adjustments (so I understand the code)
    In the MainForm make the adjustments so the buttons run

--- CityGuide/src/Panels/Activities/ActivitiesSinglePanel.java
+++ CityGuide/src/Panels/Activities/ActivitiesSinglePanel.java
@@ -5,51 +5,53 @@ import java.awt.*;
 
 public class ActivitiesSinglePanel extends JPanel {
     JLabel activityName;
-    public JButton button;
-    public JLabel label;
+    public JButton buttonSave;
+    public JButton buttonDetails;
+    public JLabel labelName;
+    public JLabel labelPicture;
+    public JLabel labelRating;
     public JTextArea textarea;
 
     public ActivitiesSinglePanel()
     {
         //activityName= new JLabel();
 
-        this.setPreferredSize(new Dimension(400,150));
+        this.setPreferredSize(new Dimension(400,175));
+        this.setMaximumSize(new Dimension(400,175));
         final boolean shouldFill = true;
         final boolean shouldWeightX = true;
         final boolean RIGHT_TO_LEFT = false;
-        this.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        if (RIGHT_TO_LEFT) {
+            this.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }
         this.setBorder(BorderFactory.createLineBorder(Color.black));
-
-
 
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
 
-        label = new JLabel("Activity Name");
+        labelName = new JLabel("Activity Name");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 0.5;
         c.insets = new Insets(5,5,5,5);  //padding
-        c.gridwidth = 2;
         c.gridx = 0;
         c.gridy = 0;
-        this.add(label, c);
+        this.add(labelName, c);
 
-        label = new JLabel("Rating");
+        labelRating = new JLabel("Rating");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 0.5;
         c.insets = new Insets(5,5,5,5);  //padding
         c.gridx = 2;
         c.gridy = 0;
-        this.add(label, c);
+        this.add(labelRating, c);
 
-        label = new JLabel("Activity Picture");
+        labelPicture = new JLabel("Activity Picture");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 0.5;
         c.insets = new Insets(5,5,5,5);  //padding
-        c.gridwidth = 2;
         c.gridx = 0;
         c.gridy = 1;
-        this.add(label, c);
+        this.add(labelPicture, c);
 
         textarea= new JTextArea("Activity Description");
         textarea.setPreferredSize(new Dimension(100,100));
@@ -57,24 +59,29 @@ public class ActivitiesSinglePanel extends JPanel {
         c.weightx = 0.5;
         c.insets = new Insets(5,5,5,5);  //padding
         c.ipady = 40;       //make this component tall
-        c.gridx = 3;
+        c.gridx = 2;
         c.gridy = 1;
         this.add(textarea, c);
 
-        button = new JButton("Details");
+        buttonDetails = new JButton("Details");
         c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.LAST_LINE_START;
         c.weightx = 0.5;
         c.insets = new Insets(5,5,5,5);  //padding
+        c.ipady = 15;
         c.gridx = 0;
         c.gridy = 2;
-        this.add(button, c);
+        this.add(buttonDetails, c);
 
-        button = new JButton("Save");
+        buttonSave = new JButton("Save");
         c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.LAST_LINE_END;
         c.weightx = 0.5;
         c.insets = new Insets(5,5,5,5);  //padding
-        c.gridx = 3;
+        c.ipady = 15;
+        c.gridx = 2;
         c.gridy = 2;
-        this.add(button, c);
+        this.add(buttonSave, c);
     }
+
 }
