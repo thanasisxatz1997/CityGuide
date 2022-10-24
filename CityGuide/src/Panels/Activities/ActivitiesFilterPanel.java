0cfe2ccab662e9fbd6fe22cc34792fe07956f7a3
thanasisxatz1997 <thanasisxatz1997@hotmail.com>
2022-10-24 20:51:32 +0300

    Main_Third_Push

--- CityGuide/src/Panels/Activities/ActivitiesFilterPanel.java
+++ CityGuide/src/Panels/Activities/ActivitiesFilterPanel.java
@@ -3,15 +3,4 @@ package Panels.Activities;
 import javax.swing.*;
 
 public class ActivitiesFilterPanel extends JPanel {
-    JComboBox comboBox5;
-    JTextField textField1;
-    JCheckBox checkBox1;
-    JButton activitiesApplyFiltersbutton;
-
-
-    public ActivitiesFilterPanel() {
-        activitiesApplyFiltersbutton = new JButton();
-        textField1 = new JTextField();
-        checkBox1 = new JCheckBox();
-    }
 }
--- CityGuide/src/Panels/Stores/StoreFilterPanel.java
+++ CityGuide/src/Panels/Stores/StoreFilterPanel.java
@@ -6,21 +6,11 @@ public class StoreFilterPanel extends JPanel {
     JButton applyFilters;
     JTextField searchField;
     JLabel searchLabel;
-    JTextField textField1;
-    JLabel typeLabel;
-    JComboBox comboBox6;
-    JLabel ratingLabel;
-    JLabel deliveryLabel;
-    JCheckBox checkBox2;
 
     public StoreFilterPanel()
     {
         applyFilters=new JButton();
         searchField=new JTextField();
-        textField1 = new JTextField();
-        comboBox6 = new JComboxBox();
-        checkBox2 = new JCheckBoc();
-
     }
 
 }
